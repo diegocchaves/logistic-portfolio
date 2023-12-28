3d7e531170220469161a468b526cdc214aebc467
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2023-12-28 18:38:52 +0100

    add a mobile layout in the social icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
 const Contact = () => {
-    return (
-        <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4' >
-            <form method='POST' action="https://getform.io/f/f437e598-9e82-4608-8df9-dff71cc633c9" className='flex flex-col max-w-[600px] w-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#28bdec] text-gray-300'>Contacto</p>
-                    <p className='py-4 text-gray-300'>// Envía el formulario a continuación o envíame un correo electrónico a diegocchaves21@gmail.com</p>
-                </div>
-                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-                <textarea className='bg-[#ccd6f6] p-2' name='massage' rows='10' placeholder='Message'></textarea>
-                <button className='text-white border-2 hover:bg-[#28bdec] hover:border-[#28bdec] px-4 py-3 my-8 mx-auto flex items-center '>Trabajemos juntos 😉</button>
-            </form>
+  return (
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4' >
+      <form method='POST' action="https://getform.io/f/f437e598-9e82-4608-8df9-dff71cc633c9" className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-[#28bdec] text-gray-300'>Contacto</p>
+          <p className='py-4 text-gray-300'>// Envía el formulario a continuación o envíame un correo electrónico a diegocchaves21@gmail.com</p>
         </div>
-    )
+        <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+        <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+        <textarea className='bg-[#ccd6f6] p-2' name='massage' rows='10' placeholder='Message'></textarea>
+        <button className='text-white border-2 hover:bg-[#28bdec] hover:border-[#28bdec] px-4 py-3 my-8 mx-auto flex items-center '>Trabajemos juntos 😉</button>
+      </form>
+    </div>
+  )
 }
 
 export default Contact
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,111 +1,141 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars, FaTimes, FaLinkedin, FaTwitter } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/images/logo copy.png'
 import { Link } from 'react-scroll'
 
-
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false)
+  const [mobile, setMobile] = useState(false)
 
-    return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px4 bg-[#0a192f] text-gray-300'>
-            <div>
-                <img src={Logo} alt="Logo image" style={{ width: '100px' }} />
-            </div>
+  useEffect(() => {
+    const handleResize = () => {
+      setMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
+    };
 
-            {/* menu */}
-            <ul className='hidden md:flex'>
-                <li>
-                    <Link to="home" smooth={true} duration={500}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="about" smooth={true} duration={500}>
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link to="skills" smooth={true} duration={500}>
-                        Skills
-                    </Link>
-                </li>
-                <li>
-                    <Link to="work" smooth={true} duration={500}>
-                        Work
-                    </Link>
-                </li>
-                <li>
-                    <Link to="contact" smooth={true} duration={500}>
-                        Contact
-                    </Link>
-                </li>
-            </ul>
+    window.addEventListener('resize', handleResize);
+    handleResize();
 
-            {/* hamburge */}
-            <div onClick={handleClick} className='z-10 md:hidden'>
-                {!nav ? <FaBars /> : <FaTimes />}
-            </div>
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-            {/* mobile menu */}
-            <ul className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>
-                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
-                        Home
-                    </Link>
-                </li>
-                <li className='py-6 text-4xl'>
-                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
-                        About
-                    </Link>
-                </li>
-                <li className='py-6 text-4xl'>
-                    <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
-                        Skills
-                    </Link>
-                </li>
-                <li className='py-6 text-4xl'>
-                    <Link onClick={handleClick} to="work" smooth={true} duration={500}>
-                        Work
-                    </Link>
-                </li>
-                <li className='py-6 text-4xl'>
-                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
-                        Contact
-                    </Link></li>
-            </ul>
+  const handleClick = () => setNav(!nav)
 
-            {/* social icons */}
-            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-                <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#4267b2]'>
-                        <a className='flex items-center justify-between w-full text-gray-300 ' href="https://www.linkedin.com/in/chaves-diego/">
-                            Linkedin <FaLinkedin size={30} />
-                        </a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1da1f2]'>
-                        <a className='flex items-center justify-between w-full text-gray-300 ' href="https://twitter.com/diego_carve">
-                            Twitter <FaTwitter size={30} />
-                        </a>
-                    </li>
-                    {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                        <a className='flex items-center justify-between w-full text-gray-300 ' href="/">
-                            Email <HiOutlineMail size={30} />
-                        </a>
-                    </li> */}
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                        <a className='flex items-center justify-between w-full text-gray-300 ' href="https://drive.google.com/file/d/1qfcAZMTdLYSH-7sobEz7jEOqdmkLLnMB/view?usp=drive_link">
-                            Currículo<BsFillPersonLinesFill size={30} />
-                        </a>
-                    </li>
-                </ul>
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt="Logo image" style={{ width: '100px' }} />
+      </div>
 
-            </div>
-        </div >
-    )
+      {/* menu */}
+      <ul className={mobile ? 'hidden' : ' hidden md:flex'}>
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* social icons */}
+      {mobile ? (
+        <div className='flex items-center mx-1'>
+          <ul className='flex flex-row items-center'>
+            <li>
+              <a href="https://www.linkedin.com/in/chaves-diego/">
+                <FaLinkedin className='mx-2 text-[#28BDEC]' size={20} />
+              </a>
+            </li>
+            <li >
+              <a href="https://twitter.com/diego_carve">
+                <FaTwitter className='mx-2 text-[#28BDEC]' size={20} />
+              </a>
+            </li>
+            <li >
+              <a href="https://drive.google.com/file/d/1qfcAZMTdLYSH-7sobEz7jEOqdmkLLnMB/view?usp=drive_link">
+                <BsFillPersonLinesFill className='mx-2 text-[#28BDEC]' size={20} />
+              </a>
+            </li>
+          </ul>
+        </div>) : (
+        <div className='hidden lg:flex flex-col top-[35%] left-0'>
+          <ul>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#4267b2]'>
+              <a className='flex items-center justify-between w-full text-gray-300 ' href="https://www.linkedin.com/in/chaves-diego/">
+                Linkedin <FaLinkedin size={30} />
+              </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1da1f2]'>
+              <a className='flex items-center justify-between w-full text-gray-300 ' href="https://twitter.com/diego_carve">
+                Twitter <FaTwitter size={30} />
+              </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+              <a className='flex items-center justify-between w-full text-gray-300 ' href="https://drive.google.com/file/d/1qfcAZMTdLYSH-7sobEz7jEOqdmkLLnMB/view?usp=drive_link">
+                Currículo<BsFillPersonLinesFill size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      {/* hamburge */}
+      <div onClick={handleClick} className='z-10 md:hidden'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* mobile menu */}
+      {mobile && (<ul className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link></li>
+      </ul>)}
+
+    </div >
+  )
 }
 
 export default Navbar
